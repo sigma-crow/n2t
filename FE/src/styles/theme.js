5d0368ca7fa6d4718a30e742c760b0f12e88506e
liendmch <mon823@naver.com>
2021-05-13 22:59:25 +0900

    [FE] add : globalStyle 및 theme 적용

---
+++ FE/src/styles/theme.js
@@ -0,0 +1,29 @@
+export const Colors = {
+  darkBrown: '#5f4837',
+  white: '#FFFFFF',
+};
+
+export const FontSize = {
+  xs: '18px',
+  sm: '24px',
+  md: '36px',
+  lg: '48px',
+};
+
+export const FontWeight = {
+  thin: 200,
+  light: 300,
+  regular: 400,
+  bold: 700,
+};
+
+export const Size = {
+  pageWidth: '1280px',
+};
+
+export const theme = {
+  color: Colors,
+  fontSize: FontSize,
+  fontWeight: FontWeight,
+  size: Size,
+};
