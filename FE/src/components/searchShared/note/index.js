9be9abe02f871a61b41f01382f37a5c2b5ee022b
ej4613 <40769926+ej4613@users.noreply.github.com>
2021-06-02 02:33:27 +0900

    [FE] add : add Note
    - 노트디자인

---
+++ FE/src/components/searchShared/note/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 300px;
+  height: 120px;
+  background: #e0d4ca;
+  border-radius: 10px;
+  box-shadow: 0px 3px 3px -2px rgba(0, 0, 0, 0.2),
+    0px 3px 4px 0px rgba(0, 0, 0, 0.14), 0px 1px 8px 0px rgba(0, 0, 0, 0.12);
+  margin: 40px 30px;
+`;
+
+const Title = styled.div`
+  font-size: ${({theme}) => theme.fontSize.sm};
+  font-weight: ${({theme}) => theme.fontWeight.bold};
+  color: black;
+  width: 200px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-left: 40px;
+  padding-top: 25px;
+`;
+
+const Writer = styled.div`
+  float: right;
+  font-size: ${({theme}) => theme.fontSize.s};
+  font-weight: ${({theme}) => theme.fontWeight.regular};
+  color: black;
+  width: 90px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  padding-right: 25px;
+  padding-top: 15px;
+`;
+
+const Note = ({note}) => {
+  return (
+    <Wrapper>
+      <Title>{note.title}</Title>
+      <Writer>{note.writer}</Writer>
+    </Wrapper>
+  );
+};
+
+export default Note;
