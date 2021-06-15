2d6c1ec5ee23fcd6881c998745c34bb1a121759c
1hyunjin <a92046503@gmai.com>
2021-06-15 21:21:44 +0900

    [FE] feat: 로그인 페이지
    로그인페이지 구현

---
+++ FE/src/components/LogIn.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import {useState} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import {makeStyles} from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import styled from 'styled-components';
+import {FcGoogle} from 'react-icons/fc';
+import ModalDialog from '@components/signup/ModalDialog';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: '90px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    background: '#AA856A',
+    color: 'white',
+    width: '200px',
+  },
+}));
+const Wrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  box-sizing: border-box;
+  justify-content: center;
+`;
+const TextWrapper = styled.div`
+  display: flex;
+  margin-left: 25px;
+`;
+
+const GLabel = styled.button`
+  position: relative;
+  display: inline-flex;
+  outline: none;
+  color: black;
+  border: none;
+  align-items: center;
+  background-color: #e5e5e5;
+  cursor: pointer;
+  border-radius: 4px;
+  width: 200px;
+  height: 36.3px;
+  margin-top: 50px;
+  padding: 0 20px;
+
+  &:hover {
+    background: #c7c4bb;
+  }
+`;
+
+const LogIn = () => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Container component='main' maxWidth='xs'>
+      <CssBaseline />
+      <div className={classes.paper}>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            label='ID'
+          />
+          <TextField
+            variant='outlined'
+            margin='normal'
+            required
+            fullWidth
+            type='password'
+            label='Password'
+          />
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2' color='inherit'>
+                ID/Password 찾기
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href='#'
+                variant='body2'
+                color='inherit'
+                onClick={handleOpen}
+              >
+                회원가입
+              </Link>
+              <ModalDialog open={open} handleClose={handleClose} />
+            </Grid>
+          </Grid>
+          <Wrapper>
+            <Button variant='contained' className={classes.submit}>
+              Log In
+            </Button>
+          </Wrapper>
+          <Wrapper>
+            <GLabel>
+              <FcGoogle size='20' />
+              <TextWrapper>Google 로그인</TextWrapper>
+            </GLabel>
+          </Wrapper>
+        </form>
+      </div>
+    </Container>
+  );
+};
+export default LogIn;
