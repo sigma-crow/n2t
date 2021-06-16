a7f5b6cef356db892eb9d171f1723eb37b7fc9ca
Im WonYong <ssassb123@daum.net>
2021-06-16 16:42:43 +0900

    [FE] add : 메인 페이지
    Home Container과 Presenter 추가

---
+++ FE/src/pages/Home/HomeContainer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import HomePresenter from './HomePresenter';
+
+export default class extends React.Component {
+  state = {};
+
+  render() {
+    return <HomePresenter />;
+  }
+}
