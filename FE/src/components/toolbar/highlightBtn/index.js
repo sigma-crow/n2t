1f207325e6c5e782bbab79a0516bd57175daebf3
ej4613 <40769926+ej4613@users.noreply.github.com>
2021-06-19 23:13:10 +0900

    [FE] fix: fix 툴바 태그 버튼
    툴바 태그 버튼 수정

--- FE/src/components/toolbar/highlightBtn/index.js
+++ FE/src/components/toolbar/highlightBtn/index.js
@@ -1,23 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 import {AiOutlineHighlight} from 'react-icons/ai';
-import ButtonBorder from '@/components/toolbar/buttonBorder';
 
 const ButtonWrapper = styled.button`
   all: unset;
   cursor: pointer;
-  padding: 5px 10px;
+  padding: 5px 7px;
   height: auto;
   width: auto;
-  position: relative;
+  &:hover,
+  &:active {
+    border: 1px solid white;
+    border-radius: 5px;
+  }
+  margin: 0 2px;
 `;
 
-const HighlightBtn = () => {
+const HighlightBtn = ({onClick}) => {
   return (
     <>
-      {/* <Line /> */}
-      <ButtonWrapper>
-        <ButtonBorder />
+      <ButtonWrapper onClick={onClick}>
         <AiOutlineHighlight size='20px' color='#ffffff' />
       </ButtonWrapper>
     </>
--- FE/src/components/toolbar/index.js
+++ FE/src/components/toolbar/index.js
@@ -5,42 +5,64 @@ import UnderlineBtn from '@/components/toolbar/underLineBtn';
 import HighlightBtn from '@/components/toolbar/highlightBtn';
 import BlueBtn from '@/components/toolbar/blueBtn';
 import RedBtn from '@/components/toolbar/redBtn';
+import SaveBtn from '@/components/toolbar/saveBtn';
+import TestBtn from '@/components/toolbar/testBtn';
 
 const Wrapper = styled.div`
-  margin: 0;
-  width: auto;
+  margin: auto 0;
+  width: 100%;
   background: #4f4f4f;
   flex: 0.1 50px;
+  flex-direction: row;
+  padding: 7px 15px 0px 10px;
+  min-width: 350px;
+`;
+
+const BtnWrapper = styled.div`
+  height: auto;
+  width: auto;
+  display: flex;
+  float: right;
   display: flex;
   flex-direction: row;
-  padding-left: 10px;
 `;
 
 const Toolbar = () => {
+  const onBold = () => {
+    console.log('bold');
+  };
+  const onHighlight = () => {
+    console.log('highlight');
+  };
+  const onUnderline = () => {
+    console.log('underline');
+  };
+  const onRed = () => {
+    console.log('red');
+  };
+  const onBlue = () => {
+    console.log('blue');
+  };
+  const onSave = () => {
+    console.log('save');
+  };
+  const onTest = () => {
+    console.log('test');
+  };
+
   return (
     <Wrapper>
-      <BoldBtn />
-      <HighlightBtn />
-      <UnderlineBtn />
-      <RedBtn />
-      <BlueBtn />
+      <BoldBtn onClick={onBold} />
+      <HighlightBtn onClick={onHighlight} />
+      <UnderlineBtn onClick={onUnderline} />
+      <RedBtn onClick={onRed} />
+      <BlueBtn onClick={onBlue} />
+      <BtnWrapper>
+        <SaveBtn onClick={onSave} />
+        <TestBtn onClick={onTest} />
+      </BtnWrapper>
     </Wrapper>
   );
 };
 
 export default Toolbar;
-
-// const Wrapper = styled.div`
-//   background: #dfd;
-//   padding: 1.6rem 0;
-//   height: 5rem;
-//   width: auto;
-//   font-size: 1.6rem;
-//   flex: 1 0 30px;
-// `;
-
-// const Toolbar = () => {
-//   return <Wrapper>toolbar</Wrapper>;
-// };
-
-// export default Toolbar;
