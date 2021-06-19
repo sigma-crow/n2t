1f47f04855b55771ebac1aded531f1ded44c076e
ej4613 <40769926+ej4613@users.noreply.github.com>
2021-06-19 17:02:34 +0900

    [FE] add: add 노트 데이터
    노트 데이터

---
+++ FE/src/data/notes/index.js
@@ -0,0 +1,139 @@
+const notes = [
+  {
+    id: 1,
+    title: '노트1',
+    writer: '저자1',
+  },
+  {
+    id: 2,
+    title: '노트2',
+    writer: '저자2',
+  },
+  {
+    id: 3,
+    title: '노트3',
+    writer: '저자3',
+  },
+  {
+    id: 4,
+    title: '노트4',
+    writer: '저자4',
+  },
+  {
+    id: 5,
+    title: '노트5',
+    writer: '저자5',
+  },
+  {
+    id: 6,
+    title: '노트6',
+    writer: '저자6',
+  },
+  {
+    id: 7,
+    title: '노트7',
+    writer: '저자7',
+  },
+  {
+    id: 8,
+    title: '노트8',
+    writer: '저자8',
+  },
+  {
+    id: 9,
+    title: '노트9',
+    writer: '저자9',
+  },
+  {
+    id: 10,
+    title: 'ggggppppppp',
+    writer: '저자10',
+  },
+  {
+    id: 11,
+    title: '노트11',
+    writer: '저자11',
+  },
+  {
+    id: 12,
+    title: '노트12',
+    writer: '저자12',
+  },
+  {
+    id: 13,
+    title: '노트13',
+    writer: '저자13',
+  },
+  {
+    id: 14,
+    title: '노트14',
+    writer: '저자14',
+  },
+  {
+    id: 15,
+    title: '노트15',
+    writer: '저자15',
+  },
+  {
+    id: 16,
+    title: '노트16',
+    writer: '저자16',
+  },
+  {
+    id: 17,
+    title: '노트17',
+    writer: '저자17',
+  },
+  {
+    id: 18,
+    title: '노트18',
+    writer: '저자18',
+  },
+  {
+    id: 19,
+    title: '노트19',
+    writer: '저자19',
+  },
+  {
+    id: 20,
+    title: '노트20',
+    writer: '저자20',
+  },
+  {
+    id: 21,
+    title: '노트21',
+    writer: '저자21',
+  },
+  {
+    id: 22,
+    title: '노트22',
+    writer: '저자22',
+  },
+  {
+    id: 23,
+    title: '노트23',
+    writer: '저자23',
+  },
+  {
+    id: 24,
+    title: '노트24',
+    writer: '저자24',
+  },
+  {
+    id: 25,
+    title: '노트25',
+    writer: '저자25',
+  },
+  {
+    id: 26,
+    title: '노트26',
+    writer: '저자26',
+  },
+  {
+    id: 27,
+    title: '노트27',
+    writer: '저자27',
+  },
+];
+
+export default notes;
