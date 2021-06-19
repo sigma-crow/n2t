ddff67786aea165551832f7f0f7ce90356eab40b
liendmch <mon823@naver.com>
2021-06-19 17:33:55 +0900

    [BE] feat : login & 회원가입 구현
    - 로그인 및 회원가입 API 구현

--- BE/app.js
+++ BE/app.js
@@ -1,29 +1,36 @@
-const createError = require("http-errors");
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
+require('module-alias/register');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passportConfig = require('@/config/passport.config');
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-const sequelize = require("./models/index").sequelize;
+const authRouter = require('@routes/auth');
+const indexRouter = require('@routes/index');
+const usersRouter = require('@routes/users');
+const { sequelize } = require('@models/index');
 
 sequelize.sync();
 
 const app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use(passportConfig.initialize());
+const jwtAuthenticate = passportConfig.authenticate('jwt', { session: false });
+
+app.use('/api/auth', authRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -34,11 +41,11 @@ app.use(function (req, res, next) {
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.render('error');
 });
 
 module.exports = app;
