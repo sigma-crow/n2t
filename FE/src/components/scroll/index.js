06bf3b15814c1f3d55e7b3db0083aef541510607
ej4613 <40769926+ej4613@users.noreply.github.com>
2021-06-19 17:12:48 +0900

    [FE] add: add 스크롤
    스크롤

---
+++ FE/src/components/scroll/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Scroll = (props) => {
+  return (
+    <div style={{overflowY: 'scroll', height: '70vh'}}>{props.children}</div>
+  );
+};
+
+export default Scroll;
