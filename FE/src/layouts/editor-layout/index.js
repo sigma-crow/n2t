4fef1e7ea200ce0afdea42d0f425fa0037b6976d
liendmch <mon823@naver.com>
2021-06-21 00:49:00 +0900

    [FE] add:editor Layout 수정
    - 누락 파일

--- FE/src/layouts/editor-layout/index.js
+++ FE/src/layouts/editor-layout/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from 'styled-components';
 import Editor from '@/components/editor';
 import Preview from '@/components/preview';
+import {getNoteById} from '@api';
 
 const Wrapper = styled.div`
   height: 100%;
@@ -10,11 +11,21 @@ const Wrapper = styled.div`
   min-height: calc(100vh - 5rem);
 `;
 
-const EditorLayout = () => {
+const EditorLayout = ({props}) => {
+  // const buf = Buffer.from('noteContext');
+  const {match} = props;
+  const {idx} = match.params;
   const [inputText, setInputText] = useState('');
+
+  useEffect(() => {
+    getNoteById(idx).then((data) => {
+      const {noteContext} = data.note;
+      setInputText(noteContext);
+    });
+  }, []);
   return (
     <Wrapper>
-      <Editor props={{inputText, setInputText}} />
+      <Editor props={{inputText, setInputText, idx}} />
       <Preview inputText={inputText} />
     </Wrapper>
   );
